8b43d9665c61971e164c7c5fe51c5cf1d20dac90
Anastasia Prokofieva <anast.ars@yandex.ru>
2020-10-22 02:52:14 +0300

    feat: implement check brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
+function checkOpen(currentBracket, configs, stack) {
+    for (let j = 0; j < configs.length; j++) {
+        if (currentBracket === configs[j].open) {
+            stack.push(configs[j])
+            return true
+        }
+    }
+    return false
+}
+
+function checkClose(currentBracket, stack) {
+    if (stack.length !== 0) {
+        let lastConfig = stack.pop()
+        if (currentBracket === lastConfig.close) {
+            return true
+        } else {
+            stack.push(lastConfig)
+            return false
+        }
+    } else {
+        return false
+    }
+}
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let configs = bracketsConfig.map(config => ({open: config[0], close: config[1]}))
+    let stack = []
+    let brackets = str.split("")
+    for (let i = 0; i < brackets.length; i++) {
+        let currentIsClose = checkClose(brackets[i], stack)
+        if(!currentIsClose) {
+            let currentIsOpen = checkOpen(brackets[i], configs, stack)
+            if(!currentIsOpen) {
+                return false
+            }
+        }
+    }
+    if (stack.length === 0) {
+        return true
+    } else {
+        return false
+    }
 }
